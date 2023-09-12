20aede136e020c626ff1dc0f721a264267c7e5bc
William Vincent <william@william-vincent.dev>
2023-09-12 15:56:16 -0400

    added basic table structure

--- db/migrations/up/create_tables.sql
+++ db/migrations/up/create_tables.sql
@@ -1,1 +1,18 @@
--- add all tables to db
+CREATE TABLE user (
+  name TEXT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE class (
+  name TEXT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE character (
+  name TEXT PRIMARY KEY NOT NULL,
+  level INTEGER NOT NULL,
+  class TEXT NOT NULL REFERENCES class(name)
+);
+
+CREATE TABLE user_character (
+  user_name TEXT NOT NULL REFERENCES user(name),
+  character_name TEXT NOT NULL REFERENCES character(name)
+);
